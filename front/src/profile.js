40ea2cc989202b073f272bbf631ad9ecc8a9f2aa
Megan Zhang <meganzhang@152.23.216.141>
2020-02-22 20:45:28 -0500

    sign up documentation

--- front/src/profile.js
+++ front/src/profile.js
@@ -10,6 +10,7 @@ function sendData(fullname, username, email, password) {
 }
 
 function signup(ev) {
+    console.log("hello");
     let fullname = document.querySelector("#fullname").value;
     let username = document.querySelector("#username").value;
     let email = document.querySelector("#email").value;
